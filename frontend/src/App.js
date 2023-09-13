615a76124edea6c25514f8fe9097d2d83f58bee8
tatianaKonovalova <tatiana.konovalova24@gmail.com>
2023-09-13 13:39:20 +0300

    final commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,22 @@
-import React from 'react'
+import { Container } from 'react-bootstrap';
+import Header from './components/Header.jsx';
+import Footer from './components/Footer.jsx';
+import { Outlet } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
   return (
-    <h1>Welcome to ProSHop</h1>
+    <>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <Outlet />
+        </Container>
+      </main>
+      <Footer />
+      <ToastContainer />
+    </>
   )
 }
 
---
+++ frontend/src/screens/HomeScreen.jsx
@@ -0,0 +1,42 @@
+import { useParams, Link } from 'react-router-dom';
+import { Row, Col } from 'react-bootstrap';
+import Product from '../components/Product';
+import Loader from '../components/Loader';
+import Message from '../components/Message';
+import { useGetProductsQuery } from '../slices/productsApiSlice';
+import Paginate from '../components/Pagination';
+import ProductCarousel from '../components/ProductCarousel';
+
+const HomeScreen = () => {
+    const { pageNumber, keyword } = useParams();
+    const { data, isLoading, error } = useGetProductsQuery({ keyword, pageNumber });
+
+    return (
+        <>
+            {!keyword ? <ProductCarousel /> : (
+                <Link to='/' className='btn btn-light mb-4'>
+                    Go Back
+                </Link>
+            )}
+            {isLoading ? (
+                <Loader />
+            ) : error ? (
+                <Message variant='danger'>{error?.data?.message || error.error}</Message>
+            ) : (
+                <>
+                    <h1>Latest Products</h1>
+                    <Row>
+                        {data.products.map((product) => (
+                            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                                <Product product={product} />
+                            </Col>
+                        ))}
+                    </Row>
+                    <Paginate pages={data.pages} page={data.page} keyword={keyword ? keyword : ''} />
+                </>
+            )}
+        </>
+    )
+}
+
+export default HomeScreen
